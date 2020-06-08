12a4705641c1a055d9f43bba178ae38c6873b826
Jeff Kim <kim2jy@hotmail.com>
2020-06-08 11:20:33 -0700

    Add JS to assign input to favArray
    co-authored-by: Cody Fritz <clanalia55@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,27 @@
 // Business Logic
 // compile user data into an array
-const favArray = [favColor, favDay, favCoffee, favVacay];
+const favArray = [1, 2, 3, 4];
 
 // User Interface Logic
 // gather data from form
 $(document).ready(function() {
-  const favColor = $("input:radio[name=color]:checked").val();
-  const favDay = $("input:radio[name=day]:checked").val();
-  const favCoffee = $("input:radio[name=coffee").val();
-  const favVacay = $("input#vacation").val();
+  $("fav-things").submit(function(event) {
+    const favColor = $("input:radio[name=color]:checked").val();
+    const favDay = $("input:radio[name=day]:checked").val();
+    const favCoffee = $("input:radio[name=coffee]:checked").val();
+    const favVacay = $("input#vacation").val();
+
+    favArray[0] = favColor;
+    console.log(favArray);
+    favArray[1] = favDay;
+    favArray[2] = favCoffee;
+    favArray[3] = favVacay;
+
+    event.preventDefault();
+  });
 
 
+  
 })
 
 
