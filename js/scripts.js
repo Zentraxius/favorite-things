0b8a1d8f6e2654b3cc3dad1f660d6744ac4541f6
Jeff Kim <kim2jy@hotmail.com>
2020-06-08 11:42:12 -0700

    Add script and HTML to output array to user
    co-authored-by: Cody Fritz <clanalia55@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,25 @@ const favArray = [1, 2, 3, 4];
 // User Interface Logic
 // gather data from form
 $(document).ready(function() {
-  $("fav-things").submit(function(event) {
+  $("#fav-things").submit(function(event) {
+
     const favColor = $("input:radio[name=color]:checked").val();
     const favDay = $("input:radio[name=day]:checked").val();
     const favCoffee = $("input:radio[name=coffee]:checked").val();
     const favVacay = $("input#vacation").val();
 
     favArray[0] = favColor;
-    console.log(favArray);
     favArray[1] = favDay;
     favArray[2] = favCoffee;
     favArray[3] = favVacay;
 
+    $("#fav-list").append("<li>" + favArray[0] + "</li>");
+    $("#fav-list").append("<li>" + favArray[1] + "</li>");
+    $("#fav-list").append("<li>" + favArray[2] + "</li>");
+    $("#fav-list").append("<li>" + favArray[3] + "</li>");
+
     event.preventDefault();
+
   });
 
 
